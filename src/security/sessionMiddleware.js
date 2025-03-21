51ba21963e948a3decc893b036682a55facffd10
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2025-03-21 23:47:27 +0000

    for some reasons using === on secure mod of cookies just break everything :/

--- src/security/sessionMiddleware.js
+++ src/security/sessionMiddleware.js
@@ -25,7 +25,7 @@ const sessionMiddleware = session({
   saveUninitialized: false, 
   cookie: {
     httpOnly: true,      
-    secure: NODE_ENV === 'production', // HTTPS only in production
+    secure: NODE_ENV == 'production', // HTTPS only in production
     maxAge: 30 * 24 * 60 * 60 * 1000,
     domain: DOMAIN,
     sameSite: 'lax'
