c0e07452a922daca671a2ed56de0ed6c7223b334
Samuele Orazio Durante <149787418+SamueleOrazioDurante@users.noreply.github.com>
2025-04-10 09:17:38 +0000

    inizio implementazione grafana e prometheus, ancora brutto, tanto

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const { promisify } = require('util');
+const prometheus = require('prom-client');
 const WebSocket = require('ws');
 
 const app = express();
@@ -17,8 +18,24 @@ const socketio = require('../web_services/socketio');
 
 const LOGS_PATH = envManager.readLogsPath();
 
+// Metrics setup
+
+// Create a Registry which registers the metrics
+const register = new prometheus.Registry();
+
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'server'
+});
+
 // Serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 
 app.get('/api/get/handle', async(req,res) => {
   const user_id = req.query.user_id;
